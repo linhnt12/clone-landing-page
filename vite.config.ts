a1367c5f9f4fec2a4e8872dcef3df03c32a19c54
Linh Nguyen <linh.nguyn80990@gmail.com>
2025-03-27 21:15:11 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+declare module "@remix-run/node" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
+
+export default defineConfig({
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  server: {
+    port: 3000, // 🔥 Thêm dòng này để đổi cổng thành 3000
+  },
+});
